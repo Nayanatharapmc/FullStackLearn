2dd9ca853e6a922eebd74a19f68ba93ee54ac9ea
Nayanatharapmc <chethmi.21@cse.mrt.ac.lk>
2025-03-04 11:52:46 +0530

    Initialize frontend

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,13 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port', PORT);
 });
